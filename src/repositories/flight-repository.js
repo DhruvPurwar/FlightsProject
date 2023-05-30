7c43c870d0c44c63bc93f392705af9ee0f7b0a02
Dhruv <dhruvpurwar15@gmail.com>
2023-05-30 12:28:03 +0530

    Added complex join for fetching airports & multi-level joins

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository = require("./crud-repository");
-const { Flights } = require("../models");
+const { Sequelize, Op } = require("sequelize");
+const { Flights, Airplane, Airport, City } = require("../models");
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -10,6 +11,47 @@ class FlightRepository extends CrudRepository {
     const response = await Flights.findAll({
       where: filter,
       order: sort,
+      //   Helps in eager Loading
+      //   We only mention foreign keys in migration, so JS layer (model) doesnt know that and by default picks up primary keys.
+      // That is y, we need to use "on" field here to justify
+      include: [
+        {
+          model: Airplane,
+          required: true,
+          as: "airplaneDetails", // also add alias while making associations
+        },
+        {
+          model: Airport,
+          required: true,
+          as: "departureAirport",
+          on: {
+            // DO mapping on different columns (rather than Airport.Id to Airport.code)
+            col1: Sequelize.where(
+              Sequelize.col("Flights.departureAirportId"),
+              "=",
+              Sequelize.col("departureAirport.code")
+            ),
+          },
+          include: {
+            model: City,
+            required: true,
+            as: "cityDetails",
+          },
+        },
+        {
+          model: Airport,
+          required: true,
+          as: "arrivalAirport",
+          on: {
+            // DO mapping on different columns (rather than Airport.Id to Airport.code)
+            col1: Sequelize.where(
+              Sequelize.col("Flights.arrivalAirportId"),
+              "=",
+              Sequelize.col("arrivalAirport.code")
+            ),
+          },
+        },
+      ],
     });
 
     return response;
